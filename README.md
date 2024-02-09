d4dffbded1dde83677dd81b2929ba9afbce0df79
ShonSagoro <ramosproque@gmail.com>
2024-02-08 20:08:41 -0600

    fix problem with color detect

---
+++ README.md
@@ -0,0 +1,17 @@
+Un espacio de color es un sistema o modelos de referencia que permiten especificar la creación, o reproducción de un
+color, es decir, estos indican la manera en la que nosotros un color está definido en ese modelo, con el fin de
+facilitar la especificación de este de una forma estandarizada o normativa.
+
+En este caso, usaremos los espacios de color para detectar los colores de una imagen, con el fin de enteder su uso.
+
+```
+.            ／＞　 フ
+            | 　_　_| 
+          ／` ミ__^ノ 
+         /　　　　 |
+        /　 ヽ　　 ﾉ        ╱|、
+       /　　 |　|　|      (˚ˎ。~7  
+／￣|　　    |　|　|       |、˜〵          
+(￣ヽ＿_     ヽ_)__)      じしˍ,)ノ  
+＼二)
+```
--- View/Main_window.py
+++ View/Main_window.py
@@ -3,15 +3,19 @@ import numpy as np
 
 
 class MainWindow():
-    umbral_bajo = np.array([0, 100, 100])
-    umbral_alto = np.array([10, 255, 255])
-    img = cv2.imread('public/color_primary.jpg')
+    umbral_bajo1 = (170, 100, 100)
+    umbral_alto1 = (179, 255, 255)
+    umbral_bajo2 = (0, 100, 100)
+    umbral_alto2 = (10, 255, 255)
+    img_original = cv2.imread('public/color_primary.jpg')
 
     def __init__(self):
-        self.img_hsv = cv2.cvtColor(self.img, cv2.COLOR_BGR2HSV)
-        self.mask = cv2.inRange(self.img_hsv, self.umbral_bajo, self.umbral_alto)
-        self.res = cv2.bitwise_and(self.img, self.img, mask=self.mask)
-        cv2.imshow('Imagen Original vs Detección de Rojo', np.hstack([self.img, self.res]))
+        self.img_hsv = cv2.cvtColor(self.img_original, cv2.COLOR_BGR2HSV)
+        mask1 = cv2.inRange(self.img_hsv, self.umbral_bajo1, self.umbral_alto1)
+        mask2 = cv2.inRange(self.img_hsv, self.umbral_bajo2, self.umbral_alto2)
+        self.mask = mask1 + mask2
+        self.res = cv2.bitwise_and(self.img_original, self.img_original, mask=self.mask)
+        cv2.imshow('Imagen Original vs Detección de Rojo', np.hstack([self.img_original, self.res]))
         while True:
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
